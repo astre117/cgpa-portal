a0042c82c0214cc77eac8b2d627658a2587b3fab
Gabriel <igwebuike129@gmail.com>
2026-06-12 15:23:59 -0900

    Try brevo port 465

--- backend/server.js
+++ backend/server.js
@@ -491,8 +491,8 @@ app.post("/api/forgot-password", async (req, res) => {
 
         const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
